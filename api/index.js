437348485809de93c56b2af958e68ca5c5aa9f7d
Maria <110576299+md-sart@users.noreply.github.com>
2025-10-07 02:13:20 +0000

    adicionando backend

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const headerJson = {
 };
 
 const instance = axios.create({
-  baseURL: "https://parseapi.back4app.com",
+  baseURL: "aos-postgree-express-awijmuxv4-marias-projects-ed92f867.vercel.app",
   timeout: 1000,
   headers: {
     "X-Parse-Application-Id": "lzQ61WWmjSxYma4dOZSVhO5Ofo9HQ0WaXT1bTRyY",
